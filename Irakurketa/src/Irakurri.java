73fc878d384c70be2d9f60e0dcb203ba981eccf6
Harkaitz-Fevi <web25hferreira@imaltuna.com>
2026-01-20 11:42:30 +0100

    Hasiera

---
+++ Irakurketa/src/Irakurri.java
@@ -0,0 +1,11 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class Irakurri {
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader())
+        }
+    }
+}
