81883196f65ce75a43709b3f8c852a4ccdb8d35e
Harkaitz-Fevi <web25hferreira@imaltuna.com>
2026-01-21 13:21:21 +0100

    Data_base

---
+++ Irakurketa/src/ProbaKonexioa.java
@@ -0,0 +1,30 @@
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ProbaKonexioa {
+
+    public static void main(String[] args) {
+
+        DBKonexioa konex = new DBKonexioa();
+        Connection cn = null;
+
+        try {
+            System.out.println("Sartu da 1");
+            cn =konex.konektatu();
+            System.out.println("Sartu da 2");
+            
+            
+            if (cn != null && !cn.isClosed()) {
+                System.out.println("Komunikazio kanala irekita dago.");
+                cn.close();
+                System.out.println("Konexioa itxi da.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Errorea datu-basera konektatzean");
+            e.printStackTrace();
+        }
+    }
+}
+
