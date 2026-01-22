4fb5739fde31c284e1ae8ce75c10111b9b7b2175
Harkaitz-Fevi <web25hferreira@imaltuna.com>
2026-01-22 09:37:28 +0100

    main eguneratu

--- Irakurketa/src/DBKonexioa.java
+++ Irakurketa/src/DBKonexioa.java
@@ -1,9 +1,5 @@
 
-<<<<<<< HEAD
-import java.sql.Connection;   
-=======
 import java.sql.Connection;
->>>>>>> dev
 import java.sql.DriverManager;   
 import java.sql.SQLException;    
 
@@ -12,19 +8,9 @@ public class DBKonexioa {
 
     private static final String URL = "jdbc:mysql://localhost:3306/maltunaDB";
     private static final String USER = "root";
-<<<<<<< HEAD
     private static final String PASS = "72546888N";
-=======
-    private static final String PASS = "72587073Q";
->>>>>>> dev
 
     public Connection konektatu() throws SQLException {
         return  DriverManager.getConnection(URL, USER, PASS);
     }
-<<<<<<< HEAD
 }
-
-
-=======
-}
->>>>>>> dev
