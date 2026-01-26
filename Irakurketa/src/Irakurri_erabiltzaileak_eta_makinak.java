a5ab25f98898da04b1cba7833322669daa0166cd
Harkaitz-Fevi <web25hferreira@imaltuna.com>
2026-01-26 08:39:17 +0100

    .csv-ren aktualizazioa

--- Irakurketa/src/Irakurri_erabiltzaileak_eta_makinak.java
+++ Irakurketa/src/Irakurri_erabiltzaileak_eta_makinak.java
@@ -2,19 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Irakurri_piezak{
+public class Irakurri_erabiltzaileak_eta_makinak{
 
     public static void main(String[] args) {
         String lerroa;
 
-        try (BufferedReader br = new BufferedReader(new FileReader("data/piezak (1).csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("data/erabiltzaileak_eta_makinak.csv"))) {
             br.readLine(); //Goiburua saltatzeko
 
                 // != null esan nahi du, ejekutatu egingo dela irakurtzeko lerroak dauden bitartean
                 // .csv-ko lerroak irakurtzen ditu amaierararte 
             while ((lerroa = br.readLine()) != null) {
-                String[] datuak = lerroa.split(",");
-                System.out.println(datuak[0] + "-" + datuak[1] + "-" + datuak[2] + "-" + datuak[3]+ "-" + datuak[4]+ "-" + datuak[5]);
+                String[] datuak = lerroa.split(",", -1);
+                System.out.println(datuak[0] + "-" + datuak[1] + "-" + datuak[2]+ "-" + datuak[3]);
             }
         } catch (IOException e) {
             System.out.println("Errorea fitxategia irakurtzean.");
--- Irakurketa/src/Irakurri_pieza_eta_makinak_lotura.java
+++ Irakurketa/src/Irakurri_pieza_eta_makinak_lotura.java
@@ -2,19 +2,19 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Irakurri_piezak{
+public class Irakurri_pieza_eta_makinak_lotura {
 
     public static void main(String[] args) {
         String lerroa;
 
-        try (BufferedReader br = new BufferedReader(new FileReader("data/piezak (1).csv"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("data/pieza_eta_makinak_lotura.csv"))) {
             br.readLine(); //Goiburua saltatzeko
 
                 // != null esan nahi du, ejekutatu egingo dela irakurtzeko lerroak dauden bitartean
                 // .csv-ko lerroak irakurtzen ditu amaierararte 
             while ((lerroa = br.readLine()) != null) {
                 String[] datuak = lerroa.split(",");
-                System.out.println(datuak[0] + "-" + datuak[1] + "-" + datuak[2] + "-" + datuak[3]+ "-" + datuak[4]+ "-" + datuak[5]);
+                System.out.println(datuak[0] + "-" + datuak[1]);
             }
         } catch (IOException e) {
             System.out.println("Errorea fitxategia irakurtzean.");
